2c011d73904e6eb4daffb486ac4edce7d64eaa21
Owen Sweet <o.sweet.work@gmail.com>
2024-08-27 05:31:00 -0700

    Added directions rendering with polyline and form input for other user location. Meetup point is found between but still needs support for different modes of travel for each party. For now this is minimum viable product

--- public/js/map.js
+++ public/js/map.js
@@ -1,39 +1,52 @@
+let map;
+var user1Marker;
+var user2Marker;
+var meetupMarker;
+
+var directionsService;
+let directionsRenderer1;
+let directionsRenderer2;
+
+var geocoder;
+
+var pannedOut = false;
+
+
 function getDirection(prevCoord, currCoord) {
   //calculations to get angle between previous position and current
   const diffLat = currCoord.lat - prevCoord.lat;
   const diffLng = currCoord.lng - prevCoord.lng;
 
   //if there is no movement, set the angle to upright
-  if (Math.abs(diffLat) < (0.1 / 111139) && Math.abs(diffLng) < (0.1 / (111320 * Math.cos(currCoord.lat * (Math.PI / 180))))) {
+  if (Math.abs(diffLat) < (1 / 111139) && Math.abs(diffLng) < (1 / (111320 * Math.cos(currCoord.lat * (Math.PI / 180))))) {
     return 0;
   }
 
   const antiClockwiseEastAngle = toDegrees(Math.atan2(diffLat, diffLng));
   const clockwiseNorthAngle = 90 - antiClockwiseEastAngle;
   return clockwiseNorthAngle;
-
-  user1Marker.content.style.transform = `rotate(45deg)`; 
 }
 
 function toDegrees(radian) {
   return (radian * 180) / Math.PI;
 }
 
-let map;
 
-let directionsService;
-let directionsRenderer;
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    geocoder = new google.maps.Geocoder();
+
     directionsService = new google.maps.DirectionsService();
-    directionsRenderer = new google.maps.DirectionsRenderer();
+    
 
-
+  //initialize map
    map = new Map(document.getElementById("map"), {
         center: {lat: 49.222177, lng: -122.686493},
         zoom: 16.5,
         tilt: 45,
+        // mapId: "25b1316f79a00934",
         mapId: "90f87356969d889c",
         disableDefaultUI: true,
         gestureHandling: "greedy",
@@ -42,7 +55,16 @@ async function initMap() {
         map.setTilt(90);
     });
 
-    // directionsRenderer.setMap(map)
+    directionsRenderer1 = new google.maps.DirectionsRenderer({
+      map: map,
+      suppressMarkers: true,
+      polylineOptions: { strokeColor: "purple"}
+    });
+    directionsRenderer2 = new google.maps.DirectionsRenderer({
+      map: map,
+      suppressMarkers: true,
+      polylineOptions: { strokeColor: "purple"}
+    })
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -53,18 +75,37 @@ async function initMap() {
               lng: position.coords.longitude,
             };
 
-            const user1Marker = new AdvancedMarkerElement({
+            user1Marker = new AdvancedMarkerElement({
               map,
               position: pos,
               title: 'Your Location',
-              content: document.createElement("div"), // This is necessary for AdvancedMarkerElement
+              content: document.createElement("div"),
             });
+            user2Marker = new AdvancedMarkerElement({
+              map,
+              position: null,
+              title: 'Their Location',
+              content: document.createElement("div"),
+            })
+            meetupMarker = new AdvancedMarkerElement({
+              map,
+              position: null,
+              title: 'Meetup spot',
+              content: document.createElement("div")
+            })
           
-            // Set the content of the marker
-            const markerContent = user1Marker.content;
-            markerContent.innerHTML = `<img src="/images/pointer.png" style="width: 50px; height: 50px;">`;
-            markerContent.style.transform = 'rotate(0deg)';
+            // set the content of the marker
+            const marker1Content = user1Marker.content;
+            marker1Content.innerHTML = `<img src="/images/pointer.png" style="width: 50px; height: 50px;">`;
+            marker1Content.style.transform = 'rotate(0deg)';
 
+            const marker2Content = user2Marker.content;
+            marker2Content.innerHTML = `<img src="/images/pointer2.png" style="width: 50px; height: 50px">`;
+            marker2Content.style.transform = 'rotate(0deg)';
+
+            const meetupMarkerContent = meetupMarker.content;
+            meetupMarkerContent.innerHTML = `<img src="/images/meetuppoint.png" style="width: 35px; height: 50px">`;
+            meetupMarkerContent.style.transform = 'rotate(0deg)';
 
             console.log(user1Marker.position);
             
@@ -86,9 +127,9 @@ async function initMap() {
               console.log("Angle: ", angle);
               user1Marker.content.style.transform = `rotate(${angle}deg)`; 
 
-              map.panTo(user1Marker.position);
-
-              
+              if (pannedOut == false) {
+                map.panTo(user1Marker.position);
+              }
 
             };
             
@@ -119,17 +160,65 @@ async function initMap() {
 }
 window.initMap = initMap;
 
-function calcRoute() {
-  var start = document.getElementById('start').value;
-  var end = document.getElementById('end').value;
+function setThem() {
+  user2Marker.position = themToLatLng();
+
+  updateMeetupPoint();
+  calcRoute(user1Marker.position, meetupMarker.position, directionsRenderer1, "WALKING");
+  calcRoute(user2Marker.position, meetupMarker.position, directionsRenderer2, "WALKING");
+
+  const bounds = new google.maps.LatLngBounds();
+  bounds.extend(user1Marker.position);
+  bounds.extend(user2Marker.position);
+  map.fitBounds(bounds);
+  pannedOut = true;
+  
+  google.maps.event.addListenerOnce(map, 'idle', function() {
+      map.setTilt(90);
+  });
+}
+
+function themToLatLng() {
+  return new google.maps.LatLng(49.23024, -122.68955);
+  //fix this geocode shit
+  var them = document.getElementById('them').value;
+  console.log("THEM LOCATION: ", them);
+  geocoder.geocode( {'address': them}, function(results, status) {
+    if (status == 'OK') {
+      console.log("GEOCODER RESULTS: ", results[0].geometry.location)
+      const themLatLng = new google.maps.LatLng(
+        results[0].geometry.location.lat,
+        results[0].geometry.location.lng
+      )
+      return themLatLng;
+    }
+  })
+}
+
+function updateMeetupPoint() {
+  const user1pos = user1Marker.position;
+  const user2pos = user2Marker.position;
+
+  if (user1pos && user2pos) {
+    const middlepos = {
+      lat: (user1pos.lat + user2pos.lat) / 2,
+      lng: (user1pos.lng + user2pos.lng) / 2
+    }
+    meetupMarker.position = middlepos;
+  }
+}
+
+function calcRoute(start, end, render, travelMode) {
   var request = {
     origin: start,
     destination: end,
-    travelMode: 'DRIVING'
+    travelMode: google.maps.TravelMode[travelMode]
   };
   directionsService.route(request, function(result, status) {
     if (status == 'OK') {
-      directionsRenderer.setDirections(result);
+      render.setDirections(result);
+    } else {
+      window.alert("Directions request failed due to " + status);
     }
   });
 }
