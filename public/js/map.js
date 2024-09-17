6076efe6e796cd68179628bff9a51930ec06189f
Owen Sweet <o.sweet.work@gmail.com>
2024-09-17 00:45:01 -0700

    Changed math for meetup point, now using distance matrix api to calculate trip duration from both users. Bug with how to return the value even though its printing inside the callback, its not returning because its a callback function. Not sure if I want to use promises. Also need to fix the math because just dividing the durations is not correct

--- public/js/map.js
+++ public/js/map.js
@@ -1,3 +1,4 @@
+//initializing global variables
 let map;
 var user1Marker;
 var user2Marker;
@@ -6,6 +7,8 @@ var meetupMarker;
 var directionsService;
 let directionsRenderer1;
 let directionsRenderer2;
+
+var distanceService;
 
 var geocoder;
 
@@ -21,6 +24,8 @@ var middlepos;
 
 var listener;
 
+// getDirection uses math to calculate the 
+// angle between the two inputted coordinates
 function getDirection(prevCoord, currCoord) {
   //calculations to get angle between previous position and current
   const diffLat = currCoord.lat - prevCoord.lat;
@@ -31,11 +36,14 @@ function getDirection(prevCoord, currCoord) {
   return clockwiseNorthAngle;
 }
 
+//converts degrees to radians using math formula
 function toDegrees(radian) {
   return (radian * 180) / Math.PI;
 }
 
 
+//Initializes the map with markers, directionsRenderer objects, and then
+//current position and using watchPosition to watch for position changes.
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
@@ -43,6 +51,8 @@ async function initMap() {
     geocoder = new google.maps.Geocoder();
 
     directionsService = new google.maps.DirectionsService();
+
+    distanceService = new google.maps.DistanceMatrixService();
 
 
   //initialize map
@@ -54,7 +64,9 @@ async function initMap() {
         mapId: "25b1316f79a00934",
         disableDefaultUI: true,
         gestureHandling: "greedy",
+        animatedZoom: true,
     });
+
 
     directionsRenderer1 = new google.maps.DirectionsRenderer({
       map: map,
@@ -166,18 +178,23 @@ async function initMap() {
 
         
       } else {
-        // Browser doesn't support Geolocation
+        // browser doesn't support Geolocation
         console.log("Browser doesn't support Geolocation");
         return
       }
 }
 window.initMap = initMap;
 
+//setThem function is called when a user2 locaiton is chosen. it sets
+//the new user2Marker positon and then fits the map view to the whole
+//route. Then renderers the route from each user to the meetup point
+//
 async function setThem(themlatlng) {
   user2Marker.position = themlatlng;
   
  
-
+//if user2Marker position is not null, call the function to update
+//the meetup point
   if (user2Marker.position) {
     await updateMeetupPoint();  
   }
@@ -196,19 +213,15 @@ async function setThem(themlatlng) {
   meTravelMode = checkMyTravelMode();
   themTravelMode = checkMyTravelMode();
   
+  //calculate route from each user coordinate to the meetup coordinate
   calcRoute(user1Marker.position, meetupMarker.position, directionsRenderer1, meTravelMode);
   calcRoute(user2Marker.position, meetupMarker.position, directionsRenderer2, themTravelMode);
 
   pannedOut = true;
-  
-
-  google.maps.event.addListener(map, 'idle', function() {
-      map.setTilt(45);
-  });
 }
 
+//themToLatLng function
 async function themToLatLng() {
-  //create data base of friends and if entered word is not in the session variable of friends then search from autocomplete
   var them = document.getElementById('them').value;
   if (them == "friend1") {
     return new google.maps.LatLng(123, 123);
@@ -217,10 +230,6 @@ async function themToLatLng() {
 }
 
 async function updateMeetupPoint() {
-  //find the meetup point in a different way, i should use
-  // a directions render to render a route from user1pos to user2pos,
-  // find the floor divide by two of the direction steps array and then
-  // set the meetupMarker position to that leg's position
 
   const user1pos = user1Marker.position;
   const user2pos = user2Marker.position;
@@ -243,11 +252,23 @@ function calcRoute(start, end, render, travelMode) {
   };
   directionsService.route(request).then((result) => {
     render.setDirections(result);
+    return result;
   })
 }
 
-//returns location of the middle leg
+//returns location of the middle point
 async function calcMeetupRoute(start, end, render) {
+
+  
+  const user1Mode = checkMyTravelMode();
+  const user2Mode = checkTheirTravelMode();
+
+  console.log("user1modw: ", user1Mode);
+
+  const user1Duration = await getDuration(start, end, user1Mode);
+  const user2Duration = await getDuration(end, start, user2Mode);
+
+
   const request = {
     origin: start,
     destination: end,
@@ -258,9 +279,26 @@ async function calcMeetupRoute(start, end, render) {
 
       const theroute = result.routes[0].legs[0];
       const totalDistance = theroute.distance.value;
+
       console.log(checkMyTravelMode());
       console.log(checkTheirTravelMode());
-      const meetupDistance = totalDistance / 2;
+
+      //there should be mutiple ratios:
+      //drive to walk is the max going negative if user2travelmode is quicker
+      //drive to bike is second
+
+      //user1 time / user 2 time
+//leg0 duration for each calcroute?
+//or distance matrix api but directions would be the same
+
+      console.log("user1inf: ", user1Duration);
+
+      let meetupRatio = user1Duration / user2Duration;
+
+
+      const meetupDistance = totalDistance * meetupRatio;
+
+
       let distanceCounter = 0;
       const steps = theroute.steps;
 
@@ -293,9 +331,14 @@ async function calcMeetupRoute(start, end, render) {
   return middlepos;
 }
 
+/*initializes the input for "them" input field and 
+attaches the google maps place API autocomplete Object to the input*/
 function initInput() {
   let input = document.getElementById("them");
   const center = user1Marker.position;
+
+  //setting non-strict bounds for the autocomplete to
+  //prioritize nearby places first
   const options = {
     bounds: {
       north: center.lat + 0.1,
@@ -317,6 +360,7 @@ function initInput() {
   })
 }
 
+//checking the user 2 travel mode
 function checkMyTravelMode() {
   var mymode;
   document.querySelectorAll('input[name="meTravel"]').forEach(radio => {
@@ -327,6 +371,7 @@ function checkMyTravelMode() {
   return mymode;
 }
 
+//checking the user 1 travel mode
 function checkTheirTravelMode() {
   var theirmode;
   document.querySelectorAll('input[name="themTravel"]').forEach(radio => {
@@ -337,12 +382,15 @@ function checkTheirTravelMode() {
   return theirmode;
 }
 
+//Get the center point between two coordinates, and multiplying
+//the lat and lng by a ratio defined in CalcMeetupRoute() function
 function interpolate(start, end, ratio) {
   const lat = start.lat() + (end.lat() - start.lat()) * ratio;
   const lng = start.lng() + (end.lng() - start.lng()) * ratio;
   return new google.maps.LatLng(lat, lng);
 }
 
+//Attaching event listener to the button for centering the map onto the user
 function initCenterButton() {
   const centerButton = document.getElementById("centerButton");
   centerButton.addEventListener('click', function() {
@@ -351,16 +399,47 @@ function initCenterButton() {
     map.panTo(user1Marker.position);
     google.maps.event.addListenerOnce(map, 'idle', function() {
       map.setZoom(18);
-      map.setTilt(90);
+      map.setTilt(70);
   });
   })
 }
 
+//Attaching event listener to button that sets map center to
+//meetup location and changes button style if no meetup is created
 function initMeetupClickable() {
   meetupMarker.addListener('click', function() {
     google.maps.event.removeListener(listener);
-    pannedOut = true;
+    pannedOut = false;
     map.panTo(meetupMarker.position);
     map.setZoom(16);
   })
+  const meetupButton = document.getElementById("meetupButton");
+  meetupButton.addEventListener('click', function() {
+    google.maps.event.removeListener(listener);
+    if (!meetupMarker.position) {
+      console.log("nomeetup");
+      meetupButton.classList.add('meetup-button');
+      setTimeout(() => {
+        meetupButton.classList.remove('meetup-button');
+      }, 500);
+    } else {
+    pannedOut = true;
+    map.panTo(meetupMarker.position);
+    map.setZoom(16);
+    }
+  })
+}
+
+async function getDuration(start, end, travelMode) {
+  await distanceService.getDistanceMatrix(
+    {
+      origins: [start],
+      destinations: [end],
+      travelMode: travelMode,
+    }, durationCallback );
+}
+
+function durationCallback(response) {
+  console.log(response.rows[0].elements[0].duration.value);
+  return response.rows[0].elements[0].duration.value;
 }
